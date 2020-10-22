b6d15e41ea460aaad8c470853d74e499758d5ca0
Pankrusheva Anastasiia <a.pankrusheva@gmail.com>
2020-10-22 22:02:46 +0300

    menu templates

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import './styles.css';
+import './css/styles.css';
 import './theme-switch-toggle';
 import './menu.json';
+import menuItem from './templates/menu-item.hbs';
+import dishes from './menu.json'
+
+//console.log(menuItem(dishes));
+
+const menuContainer = document.querySelector('.js-menu');
+const cardsMarkup = createMenuCardsMarkup(dishes);
+
+menuContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createMenuCardsMarkup(dishes) {
+    return dishes.map(dish => menuItem(dish)).join('');
+}
